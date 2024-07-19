cc02d1a094d7c1823576fdb77875c2f781dc1838
Achraf LOUIZA <achraf.louiza1@starteckinnov.com>
2024-07-19 23:23:58 +0200

    small modfi

--- src/train.py
+++ src/train.py
@@ -2,11 +2,12 @@ import random
 import numpy as np
 import os
 import torch as torch
-from load_data import load_EOD_data
-from evaluator import evaluate
-from model import get_loss, StockMixer
+from src.load_data import load_EOD_data
+from src.evaluator import evaluate
+from src.model import get_loss, StockMixer
 import pickle
-
+from tqdm import tqdm
+import pandas as pd
 
 np.random.seed(123456789)
 torch.random.manual_seed(12345678)
@@ -28,9 +29,9 @@ alpha = 0.1
 scale_factor = 3
 activation = 'GELU'
 
-dataset_path = '../dataset/' + market_name
+dataset_path = 'dataset/' + market_name
 if market_name == "SP500":
-    data = np.load('../dataset/SP500/SP500.npy')
+    data = np.load('dataset/SP500/SP500.npy')
     data = data[:, 915:, :]
     price_data = data[:, :, -1]
     mask_data = np.ones((data.shape[0], data.shape[1]))
@@ -50,23 +51,8 @@ else:
     with open(os.path.join(dataset_path, "price_data.pkl"), "rb") as f:
         price_data = pickle.load(f)
 
-trade_dates = mask_data.shape[1]
-model = StockMixer(
-    stocks=stock_num,
-    time_steps=lookback_length,
-    channels=fea_num,
-    market=market_num,
-    scale=scale_factor
-).to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-best_valid_loss = np.inf
-best_valid_perf = None
-best_test_perf = None
-batch_offsets = np.arange(start=0, stop=valid_index, dtype=int)
-
-
-def validate(start_index, end_index):
+def validate(model, start_index, end_index):
     with torch.no_grad():
         cur_valid_pred = np.zeros([stock_num, end_index - start_index], dtype=float)
         cur_valid_gt = np.zeros([stock_num, end_index - start_index], dtype=float)
@@ -74,6 +60,7 @@ def validate(start_index, end_index):
         loss = 0.
         reg_loss = 0.
         rank_loss = 0.
+        res = pd.DataFrame()
         for cur_offset in range(start_index - lookback_length - steps + 1, end_index - lookback_length - steps + 1):
             data_batch, mask_batch, price_batch, gt_batch = map(
 
@@ -83,6 +70,9 @@ def validate(start_index, end_index):
             prediction = model(data_batch)
             cur_loss, cur_reg_loss, cur_rank_loss, cur_rr = get_loss(prediction, gt_batch, price_batch, mask_batch,
                                                                      stock_num, alpha)
+            resi = pd.DataFrame({'prediction': prediction[:, 0], 'ground_truth': gt_batch[:, 0]})
+            resi['id'] = cur_offset
+            res = pd.concat([res, resi], ignore_index=True)
             loss += cur_loss.item()
             reg_loss += cur_reg_loss.item()
             rank_loss += cur_rank_loss.item()
@@ -93,7 +83,7 @@ def validate(start_index, end_index):
         reg_loss = reg_loss / (end_index - start_index)
         rank_loss = rank_loss / (end_index - start_index)
         cur_valid_perf = evaluate(cur_valid_pred, cur_valid_gt, cur_valid_mask)
-    return loss, reg_loss, rank_loss, cur_valid_perf
+    return loss, reg_loss, rank_loss, cur_valid_perf, res
 
 
 def get_batch(offset=None):
@@ -108,46 +98,53 @@ def get_batch(offset=None):
         np.expand_dims(price_data[:, offset + seq_len - 1], axis=1),
         np.expand_dims(gt_data[:, offset + seq_len + steps - 1], axis=1))
 
-
-for epoch in range(epochs):
-    print("epoch{}##########################################################".format(epoch + 1))
-    np.random.shuffle(batch_offsets)
-    tra_loss = 0.0
-    tra_reg_loss = 0.0
-    tra_rank_loss = 0.0
-    for j in range(valid_index - lookback_length - steps + 1):
-        data_batch, mask_batch, price_batch, gt_batch = map(
-            lambda x: torch.Tensor(x).to(device),
-            get_batch(batch_offsets[j])
-        )
-        optimizer.zero_grad()
-        prediction = model(data_batch)
-        cur_loss, cur_reg_loss, cur_rank_loss, _ = get_loss(prediction, gt_batch, price_batch, mask_batch,
-                                                            stock_num, alpha)
-        cur_loss = cur_loss
-        cur_loss.backward()
-        optimizer.step()
-
-        tra_loss += cur_loss.item()
-        tra_reg_loss += cur_reg_loss.item()
-        tra_rank_loss += cur_rank_loss.item()
-    tra_loss = tra_loss / (valid_index - lookback_length - steps + 1)
-    tra_reg_loss = tra_reg_loss / (valid_index - lookback_length - steps + 1)
-    tra_rank_loss = tra_rank_loss / (valid_index - lookback_length - steps + 1)
-    print('Train : loss:{:.2e}  =  {:.2e} + alpha*{:.2e}'.format(tra_loss, tra_reg_loss, tra_rank_loss))
-
-    val_loss, val_reg_loss, val_rank_loss, val_perf = validate(valid_index, test_index)
-    print('Valid : loss:{:.2e}  =  {:.2e} + alpha*{:.2e}'.format(val_loss, val_reg_loss, val_rank_loss))
-
-    test_loss, test_reg_loss, test_rank_loss, test_perf = validate(test_index, trade_dates)
-    print('Test: loss:{:.2e}  =  {:.2e} + alpha*{:.2e}'.format(test_loss, test_reg_loss, test_rank_loss))
-
-    if val_loss < best_valid_loss:
-        best_valid_loss = val_loss
-        best_valid_perf = val_perf
-        best_test_perf = test_perf
-
-    print('Valid performance:\n', 'mse:{:.2e}, IC:{:.2e}, RIC:{:.2e}, prec@10:{:.2e}, SR:{:.2e}'.format(val_perf['mse'], val_perf['IC'],
-                                                     val_perf['RIC'], val_perf['prec_10'], val_perf['sharpe5']))
-    print('Test performance:\n', 'mse:{:.2e}, IC:{:.2e}, RIC:{:.2e}, prec@10:{:.2e}, SR:{:.2e}'.format(test_perf['mse'], test_perf['IC'],
-                                                                            test_perf['RIC'], test_perf['prec_10'], test_perf['sharpe5']), '\n\n')
+def train(model):
+    trade_dates = mask_data.shape[1]
+    best_valid_loss = np.inf
+    best_valid_perf = None
+    best_test_perf = None
+    batch_offsets = np.arange(start=0, stop=valid_index, dtype=int)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    for epoch in range(epochs):
+        print("epoch{}##########################################################".format(epoch + 1))
+        np.random.shuffle(batch_offsets)
+        tra_loss = 0.0
+        tra_reg_loss = 0.0
+        tra_rank_loss = 0.0
+        for j in tqdm(range(valid_index - lookback_length - steps + 1)):
+            data_batch, mask_batch, price_batch, gt_batch = map(
+                lambda x: torch.Tensor(x).to(device),
+                get_batch(batch_offsets[j])
+            )
+            optimizer.zero_grad()
+            prediction = model(data_batch)
+            cur_loss, cur_reg_loss, cur_rank_loss, _ = get_loss(prediction, gt_batch, price_batch, mask_batch,
+                                                                stock_num, alpha)
+            cur_loss = cur_loss
+            cur_loss.backward()
+            optimizer.step()
+    
+            tra_loss += cur_loss.item()
+            tra_reg_loss += cur_reg_loss.item()
+            tra_rank_loss += cur_rank_loss.item()
+        tra_loss = tra_loss / (valid_index - lookback_length - steps + 1)
+        tra_reg_loss = tra_reg_loss / (valid_index - lookback_length - steps + 1)
+        tra_rank_loss = tra_rank_loss / (valid_index - lookback_length - steps + 1)
+        print('Train : loss:{:.2e}  =  {:.2e} + alpha*{:.2e}'.format(tra_loss, tra_reg_loss, tra_rank_loss))
+    
+        val_loss, val_reg_loss, val_rank_loss, val_perf, val_res = validate(model, valid_index, test_index)
+        print('Valid : loss:{:.2e}  =  {:.2e} + alpha*{:.2e}'.format(val_loss, val_reg_loss, val_rank_loss))
+    
+        test_loss, test_reg_loss, test_rank_loss, test_perf, test_res = validate(model, test_index, trade_dates)
+        print('Test: loss:{:.2e}  =  {:.2e} + alpha*{:.2e}'.format(test_loss, test_reg_loss, test_rank_loss))
+    
+        if val_loss < best_valid_loss:
+            best_valid_loss = val_loss
+            best_valid_perf = val_perf
+            best_test_perf = test_perf
+    
+        print('Valid performance:\n', 'mse:{:.2e}, IC:{:.2e}, RIC:{:.2e}, prec@10:{:.2e}, SR:{:.2e}'.format(val_perf['mse'], val_perf['IC'],
+                                                         val_perf['RIC'], val_perf['prec_10'], val_perf['sharpe5']))
+        print('Test performance:\n', 'mse:{:.2e}, IC:{:.2e}, RIC:{:.2e}, prec@10:{:.2e}, SR:{:.2e}'.format(test_perf['mse'], test_perf['IC'],
+                                                                                                           test_perf['RIC'], test_perf['prec_10'], test_perf['sharpe5']), '\n\n')
+        return val_res, test_res
